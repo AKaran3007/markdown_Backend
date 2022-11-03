719432df1069189cc233dbf6cb5390565a55049b
karan <akaran3007@gmail.com>
2022-11-03 16:07:14 +0530

    second commit

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.use(express.json());
 const cors = require ("cors");
 app.use(cors());
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "QWERTY()YTREWQASDF@QWEDCVRTY[]@@#QWERTT$QSTDD99NN33M"
 
 const mongoUrl = "mongodb+srv://aspkaran:karananand123@cluster0.mpe9ubl.mongodb.net/test"
 mongoose.connect(mongoUrl , {
@@ -41,6 +44,42 @@ app.post("/register" , async(req,res) => {
        }
    });
 
+   app.post("/login-user" , async(req , res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        return res.json({ error: "User Not Found"})
+    }
+
+    if(await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({} , JWT_SECRET);
+
+        if(res.status(201)) {
+            return res.json({ status:"ok" , data:token});
+        } else {
+            return res.json({ error:"error"});
+        }
+    }
+    res.json({ status:"error" , error:"Invalid Password"});
+   });
+
+   app.post("/userData" , async (req , res) => {
+    const {token} = req.body;
+    try {
+        const user = jwt.verify(token,JWT_SECRET);
+        const useremail = user.email;
+        User.findOne ({ email: useremail }) 
+        .then((data) => {
+            res.send({ status:"ok" , data: data});
+        })
+        .catch((error) => {
+            res.send({ status:"ok" , data: error});
+        });
+    } catch (error) {
+        
+    }
+   })
 
 app.listen(5000 , () => {
     console.log("Server Started");
